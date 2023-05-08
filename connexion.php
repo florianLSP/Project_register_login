ff61a9d5af8df19c390aaf46553907a5b7436972
Florian <florian.lesp@gmail.com>
2023-05-08 22:37:46 +0200

    add pico css

--- connexion.php
+++ connexion.php
@@ -9,7 +9,7 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="style.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
     <title>Document</title>
 </head>
 
