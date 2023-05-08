c3ce96244c16e9441176a8d5fb7c2b7c1c613e6a
Florian <florian.lesp@gmail.com>
2023-05-08 21:37:32 +0200

    charbon

---
+++ inscription.php
@@ -0,0 +1,59 @@
+<?php
+include("database.php");
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="inscription.php" method="post">
+        <label>Pseudo: </label>
+        <input type="text" name="pseudo"><br>
+
+        <label>Mot de passe:</label>
+        <input type="password" name="mdp"><br>
+
+        <input type="submit" name="login" value="S'inscrire"><br>
+    </form>
+
+</body>
+
+</html>
+
+<?php
+
+
+if (isset($_POST["login"])) {
+
+    if (!empty($_POST["pseudo"]) && !empty($_POST["mdp"])) {
+        $pseudo = $_POST["pseudo"];
+        $mdp = $_POST["mdp"];
+        $hash = password_hash($mdp, PASSWORD_DEFAULT);
+
+        $_SESSION["pseudo"] = $pseudo;
+        $_SESSION["mdp"] = $mdp;
+
+        $sql = "INSERT INTO users (utilisateur, mdp) VALUES ('$pseudo', '$hash')";
+
+        try {
+            mysqli_query(mysqli_connect($db_server, $db_user, $db_pass, $db_name), $sql);
+            echo "L'utilisateur {$pseudo} est enregistré. <br>";
+        } catch (mysqli_sql_exception) {
+            echo "Erreur lors de l'insert d'utilisateur. <br>";
+        }
+        mysqli_close( mysqli_connect($db_server, $db_user, $db_pass, $db_name));
+
+        header("Location: index.php");
+    } else {
+        echo "Il y a un soucis avec votre identifiant et/ou votre mot de passe, merci de bien vouloir réssayer.";
+    }
+}
+
+?>
